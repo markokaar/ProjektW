c13fc9f08cfbf2322d534e1222c3ef7e96a35719
AkoTonissoo <ako.tonissoo@gmail.com>
2016-03-14 20:46:45 +0200

    asd

--- src/main.java
+++ src/main.java
@@ -1,8 +1,53 @@
-/**
- * Created by Marko on 14 Mar 2016.
- */
+import java.io.FileReader;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class main {
+
     public static void main(String[] args) {
-        System.out.println("Sup?");
+        JSONParser parser = new JSONParser();
+
+        try {
+
+            Object obj = parser.parse(new FileReader(
+                    "C:/Users/Ako oma/Documents/json2.txt"));
+
+            JSONObject jsonObject = (JSONObject) obj;
+
+            String name = (String) jsonObject.get("name");
+            String base = (String) jsonObject.get("base");
+            //long id = (long) jsonObject.get("id");
+            //long cod = (long) jsonObject.get("cod");
+            //long dt = (long) jsonObject.get("dt");
+            JSONObject coord = (JSONObject) jsonObject.get("coord");
+            JSONObject main = (JSONObject) jsonObject.get("main");
+            JSONObject wind = (JSONObject) jsonObject.get("wind");
+            JSONObject clouds = (JSONObject) jsonObject.get("clouds");
+            JSONObject sys = (JSONObject) jsonObject.get("sys");
+            JSONArray weatherList = (JSONArray) jsonObject.get("weather");
+
+            System.out.println("Name: " + name);
+            System.out.println("base: " + base);
+            //System.out.println("id: " + id);
+            //System.out.println("cod: " + cod);
+            //System.out.println("dt: " + dt);
+            System.out.println("wind: " + wind);
+            System.out.println("main: " + main);
+            System.out.println("clouds: " + clouds);
+            System.out.println("sys: " + sys);
+            System.out.println("Coord: " + coord);
+            System.out.println("main: temp: " + main.get("temp"));
+            System.out.println("weatherList:");
+            Iterator<JSONObject> iterator = weatherList.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
