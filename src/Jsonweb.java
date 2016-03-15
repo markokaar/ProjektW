ff4e8f0978b1143212ad136428ec90be62ab75a8
Marko <kaarmarko@gmail.com>
2016-03-16 00:41:14 +0200

    getting information and printing stuff out

--- src/Jsonweb.java
+++ src/Jsonweb.java
@@ -4,15 +4,11 @@ import java.net.URL;
 import java.net.URLConnection;
 
 public class Jsonweb {
-
     private String city;
 
     Jsonweb(String city){
         this.city = city;
     }
-
-
-
     String getCity(){
         return city;
     }
@@ -21,7 +17,7 @@ public class Jsonweb {
     String getData() {
         String andmed;
         try {
-            String l = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=823bad53de621e0ddbdad4bbe1f29ad2";
+            String l = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=823bad53de621e0ddbdad4bbe1f29ad2";
             URL link = new URL(l);
             URLConnection connect = link.openConnection();
             BufferedReader input = new BufferedReader(new InputStreamReader(connect.getInputStream()));
