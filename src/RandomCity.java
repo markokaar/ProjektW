40210a9e26ae221019ff6b9429084b516742e10e
AkoTonissoo <ako.tonissoo@gmail.com>
2016-03-16 02:42:51 +0200

    ako

--- src/RandomCity.java
+++ src/RandomCity.java
@@ -1,32 +1,55 @@
 import java.util.concurrent.ThreadLocalRandom;
+import java.net.*;
+import java.io.*;
 
 public class RandomCity {
 
     String find() {
+        String suvalinn;
         try {
-            java.io.File fail = new java.io.File("C:/Users/Marko/Downloads");
-            java.util.Scanner sc = new java.util.Scanner(fail, "UTF-8");
+            //java.io.File fail = new java.io.File("C:/Users/Marko/Downloads");
+            //java.util.Scanner sc = new java.util.Scanner(fail, "UTF-8");
             int arv = ThreadLocalRandom.current().nextInt(0, 20000 + 1);
 
-            if (!fail.exists())
-                System.out.println("Faili ei leidnud!");
+            URL linnad = new URL("http://openweathermap.org/help/city_list.txt");
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(linnad.openStream()));
+
+            String inputLine;
+            String e;
+            int a = 0;
+            while ((inputLine = in.readLine()) != null && a != arv) {
+                a++;
+                if (a == arv) {
+                    suvalinn = inputLine.split("\t")[1];
+                    //System.out.println(suva_linn);
+                    break;
+                }
+
+                //System.out.println((inputLine.split("\t")[1]));
+                in.close();
+            }
+
+            //if (!fail.exists())
+            //    System.out.println("Faili ei leidnud!");
 
 
 
             // copytud praxi näitest
-            while (sc.hasNextLine()) {
-                String rida = sc.nextLine();
-                String[] tükid = rida.split(" ");
-                System.out.print(tükid[tükid.length-2]+" "+tükid[tükid.length-1]);
-                System.out.println();
-            }
+            //while (sc.hasNextLine()) {
+            //    String rida = sc.nextLine();
+            //    String[] tükid = rida.split(" ");
+            //    System.out.print(tükid[tükid.length-2]+" "+tükid[tükid.length-1]);
+            //    System.out.println();
+            //}
 
 
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
+            suvalinn = "error";
         }
 
-        return "tartuasd";
+        return suvalinn;
     }
 }
